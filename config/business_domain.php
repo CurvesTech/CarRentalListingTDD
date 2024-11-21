ec4afa1f38d520209f8c81409ae5d73221b9609f
Mubashar Abbas <mubashar.abbas@digitaltolk.com>
2024-11-21 20:36:53 +0500

    created essential tables

---
+++ config/business_domain.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'shapes' => ['sedan', 'hatchback', 'suv', 'crossover', 'coupe', 'convertible', 'wagon', 'van', 'truck', 'minivan', 'bus', 'pickup', 'offroad', 'other']
+];
---
+++ database/factories/CarModelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Maker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CarModel>
+ */
+class CarModelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'maker_id' => Maker::factory(),
+            'title' => $this->faker->name,
+            'shape' => $this->faker->randomElement(config('business_domain.shapes')),
+        ];
+    }
+}
